dc8c3c41f813fefa2124fa38479f6b4178be7c73
Valentyn <wolf.valentyn@gmail.com>
2022-07-16 23:33:30 +0300

    add the sidepanel color changing after scroll 390px

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,17 @@ const counters = document.querySelectorAll('.skills__ratings-counter');
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){  
+        if ($(window).scrollTop() > 390 ){
+            $(".sidepanel__icon").css("color", "rgb(6, 35, 4)");
+			$(".sidepanel__divider").css("background-color", "rgb(6, 35, 4)");
+			$(".sidepanel__text").css("color", "rgb(6, 35, 4)");
+		}  else {
+			$(".sidepanel__icon").css("color", "white");
+			$(".sidepanel__divider").css("background-color", "white");
+			$(".sidepanel__text").css("color", "white");
+		} 
+    });
+}); 
